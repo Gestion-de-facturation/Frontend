f17f9106ea75f5ef2290b7a3ebdcee178c131420
HRojotiana <hei.rojotiana@gmail.com>
2025-07-04 13:45:51 +0300

    chore: design sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,33 +3,34 @@
 import { useRouter } from 'next/navigation';
 import '@/styles/sidebar.css'
 import { LogOut } from 'lucide-react';
+import { MdReceipt, MdReceiptLong, MdPerson  } from "react-icons/md";
 
 
 export default function Sidebar() {
     const router = useRouter();
 
     return (
-        <aside className="flex flex-col w-48 bg-gray-100 text-white h-screen p-4 border-r-1 border-[#14446c]">
-            <div className='border-b-1 border-[#14446c] h-12'>
+        <aside className="flex flex-col w-48 bg-gray-100 text-white h-screen p-4 border-r-1 border-[#CCCCCC]">
+            <div className='border-b-1 border-[#CCCCCC] h-16'>
                 <h2 className="text-3xl font-semibold place-self-center logo">
                     <strong className='text-[#14446c]'>Best</strong>
                     <strong className='text-[#f18c08]'>place</strong>
                 </h2>
             </div>
-            <ul className="space-y-2 place-self-center">
-                <div className='sidebar-links'>
-                    <li><a href="/dashboard" className="text-[#14446c] text-xl">Commander</a></li>
+            <ul className="place-self-center">
+                <div className='sidebar-links mt-link'>
+                    <li><a href="/dashboard" className="flex text-[#14446c] text-lg side-bar-content hover:text-[#f18c08]"><MdReceipt className='w-5 h-5 link-icon'/>Commander</a></li>
                 </div>
                 <div className='sidebar-links'>
-                    <li><a href="/orders" className="text-[#14446c]  text-xl">Commandes</a></li>
+                    <li><a href="/orders" className=" flex text-[#14446c]  text-lg side-bar-content hover:text-[#f18c08]"><MdReceiptLong className='w-5 h-5 link-icon'/>Commandes</a></li>
                 </div>
                 <div className='sidebar-links'>
-                    <li><a href="/customers" className="text-[#14446c]  text-xl">Clients</a></li>
+                    <li><a href="/customers" className="flex text-[#14446c]  text-lg side-bar-content hover:text-[#f18c08]"> <MdPerson className='w-5 h-5 link-icon'/> Clients</a></li>
                 </div>
             </ul>
             <div className='logout-btn-container'>
                 <button
-                className=" flex justify-evenly bg-transparent text-red-600 font-semibold p-2 cursor-pointer rounded-md h-10 w-36 disconnect-btn"
+                className=" flex gap-1 bg-transparent text-red-600 font-semibold p-2 cursor-pointer rounded-md h-10 w-36 disconnect-btn"
                 onClick={() => {
                 localStorage.removeItem('isLoggedIn');
                 router.push('/login');
