524364e657cd798228e462d6b4528564feecc413
HRojotiana <hei.rojotiana@gmail.com>
2025-07-08 14:14:43 +0300

    feat: edit orders

---
+++ src/components/tables/OrderEditorTable.tsx
@@ -0,0 +1,145 @@
+'use client'
+
+import { useState } from "react";
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import CommandeTable from "./OrderTable";
+import ProductTable from "./ProductTable";
+import { ProduitCommande, Produit } from "@/utils/types/productList";
+import { FaSearch } from "react-icons/fa";
+import '@/styles/order.css'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+type Props = {
+  dataProduits: Produit[];
+  pagination: any;
+  setPagination: any;
+  globalFilter: string;
+  setGlobalFilter: (val: string) => void;
+  produitsCommandes: ProduitCommande[];
+  setProduitsCommandes: (val: ProduitCommande[]) => void;
+};
+
+export default function OrderEditor({
+  dataProduits,
+  pagination,
+  setPagination,
+  globalFilter,
+  setGlobalFilter,
+  produitsCommandes,
+  setProduitsCommandes,
+}: Props) {
+    const [commandeId, setCommandeId] = useState('');
+    const [commandeData, setCommandeData] = useState<any>(null);
+    const [adresseLivraison, setAdresseLivraison] = useState('');
+    const [adresseFacturation, setAdresseFacturation] = useState('');
+    const [fraisDeLivraison, setFraisDeLivraison] = useState(0);
+
+    const fetchCommande = async () => {
+        try {
+            const res = await axios.get(`${API_URL}/orders/${commandeId}`);
+            const data = res.data;
+            setCommandeData(data);
+            setAdresseLivraison(data.adresse_livraison);
+            setAdresseFacturation(data.adresse_facturation);
+            setFraisDeLivraison(data.frais_de_livraison || 0);
+            setProduitsCommandes(
+                data.commandeProduits.map((item: any) => ({
+                    idProduit: item.idProduit,
+                    nom: item.produit.nom,
+                    quantite: item.quantite,
+                    prixUnitaire: item.produit.prixUnitaire,
+                }))
+            );
+            toast.success('Commande chargée');
+        } catch (err) {
+            toast.error('Commande introuvable');
+        }
+    };
+
+    const updateCommande = async () => {
+        if (!adresseLivraison || !adresseFacturation || produitsCommandes.length === 0) {
+            toast.error('Vauillez remplir toutes les informations nécessaires.');
+            return ;
+        }
+        try {
+            const body = {
+                adresse_livraison: adresseLivraison,
+                adresse_facturation: adresseFacturation,
+                frais_de_livraison: Number(fraisDeLivraison),
+                produits: produitsCommandes.map((p) => ({
+                    idProduit: p.idProduit,
+                    quantite: p.quantite,
+                })),
+            };
+
+            await axios.put(`${API_URL}/orders/order/${commandeId}`, body);
+            toast.success('Commande mise à jour avec succès !');
+        } catch (err) {
+            console.error(err);
+            toast.error('Erreur lors de la mise à jour');
+        }
+    };
+
+    const removeProduct = (idProduit: string) => {
+        const updatedList = produitsCommandes.filter((p) => p.idProduit !== idProduit);
+        setProduitsCommandes(updatedList);
+    };
+
+    return (
+        <div className="space-y-4">
+            <div className="flex gap-4  mts">
+                <input 
+                type="text" 
+                placeholder="Référence de la commande"
+                className="border p-2 rounded w-64 h-10"
+                value={commandeId}
+                onChange={(e) => setCommandeId(e.target.value)}
+                />
+                <button
+                onClick={fetchCommande}
+                className=" px-4 py-2 rounded"
+                title="Rechercher"
+                >
+                    <FaSearch className="w-5 h-5"/>
+                </button>
+            </div>
+
+            {commandeData && (
+                <>
+                    <div className="grid grid-cols-2 gap-4 mts">
+                        <textarea 
+                        className="border p-2 rounded"
+                        placeholder="Adresse de livraison"
+                        value={adresseLivraison}
+                        onChange={(e) => setAdresseLivraison(e.target.value)}
+                        />
+                        <textarea 
+                        className="border p-2 rounded"
+                        placeholder="Adresse de facturation"
+                        value={adresseFacturation}
+                        onChange={(e) => setAdresseFacturation(e.target.value)}
+                        />
+                    </div>
+
+                    <CommandeTable 
+                    produitsCommandes={produitsCommandes}
+                    removeProduct={removeProduct}
+                    fraisDeLivraison={fraisDeLivraison}
+                    setFraisDeLivraison={setFraisDeLivraison}
+                    />
+
+                    <button 
+                    onClick={updateCommande}
+                    className="bg-[#f18c08] text-white text-md rounded-md w-24 h-12 mts cursor-pointer self-end order-btn"
+                    >
+                        Enregistrer
+                    </button>
+                    
+                </>
+            )}
+        </div>
+    )
+}
+
--- src/components/tables/ProductsList.tsx
+++ src/components/tables/ProductsList.tsx
@@ -5,55 +5,58 @@ import { useProducts } from '@/utils/hooks/useProducts';
 import { Produit, ProduitCommande } from '@/utils/types/productList';
 import ProductTable from './ProductTable';
 import CommandeTable from './OrderTable';
-import UserInfo from '../user/UserInfo';
+import OrderEditor from './OrderEditorTable';
 import toast from 'react-hot-toast';
 import axios from 'axios';
-import { MdListAlt, MdAdd } from "react-icons/md";
+import { MdListAlt, MdAdd, MdOutlineEdit  } from "react-icons/md";
 import '@/styles/order.css';
 
 export default function ProductsList() {
   const data = useProducts();
   const [globalFilter, setGlobalFilter] = useState('');
-  const [produitsCommandes, setProduitsCommandes] = useState<ProduitCommande[]>([]);
+  const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 5 });
+  const [activePanel, setActivePanel] = useState<'create' | 'edit' | null>(null);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  //Créer une commande
+  const [produitsCommandesCreate, setProduitsCommandesCreate] = useState<ProduitCommande[]>([]);
   const [adresseLivraison, setAdresseLivraison] = useState('');
   const [adresseFacturation, setAdresseFacturation] = useState('');
   const [fraisDeLivraison, setFraisDeLivraison] = useState<number>(0);
-  const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 5 });
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  const addProduct = (produit: Produit, quantite: number) => {
-  const exist = produitsCommandes.find(p => p.idProduit === produit.id);
-
-  if (exist) {
-    toast.error(`${produit.nom} est déjà dans la commande`);
-    return;
-  }
-
-  if (!quantite || quantite <= 0) {
-    toast.error("Quantité invalide");
-    return;
-  }
-
-  setProduitsCommandes(prev => [
-    ...prev,
-    {
+  //Modifier une commande
+  const [produitsCommandesEdit, setProduitsCommandesEdit] = useState<ProduitCommande[]>([]);
+  
+  const addProductCreate = (produit: Produit, quantite: number) => {
+    const exist = produitsCommandesCreate.find(p => p.idProduit === produit.id);
+    if (exist) return toast.error(`${produit.nom} déjà ajouté`);
+    if (!quantite || quantite <= 0) return toast.error('Quantité invalide');
+  
+    setProduitsCommandesCreate(prev => [...prev, {
       idProduit: produit.id,
       nom: produit.nom,
       quantite,
       prixUnitaire: produit.prixUnitaire
-    }
-  ]);
-
-  toast.success(`${quantite} ${produit.nom} ajouté à la commande`);
-};
-
-
-  const removeProduct = (idProduit: string) => {
-    setProduitsCommandes(prev => prev.filter(p => p.idProduit !== idProduit));
+    }]);
+    toast.success(`${quantite} ${produit.nom} ajouté`);
   };
 
-  const order = async () => {
-    if (!adresseLivraison || !adresseFacturation || produitsCommandes.length === 0) {
+  const addProductEdit = (produit: Produit, quantite: number) => {
+    const exist = produitsCommandesEdit.find(p => p.idProduit === produit.id);
+    if (exist) return toast.error(`${produit.nom} déjà ajouté`);
+    if (!quantite || quantite <= 0) return toast.error('Quantité invalide');
+
+    setProduitsCommandesEdit(prev => [...prev, {
+      idProduit: produit.id,
+      nom: produit.nom,
+      quantite,
+      prixUnitaire: produit.prixUnitaire
+    }]);
+    toast.success(`${quantite} ${produit.nom} ajouté`);
+  };
+
+  const orderCreate = async () => {    
+    if (!adresseLivraison || !adresseFacturation || produitsCommandesCreate.length === 0) {
       toast.error('Veuillez remplir toutes les informations nécessaires et ajouter au moins un produit');
       return;
     }
@@ -64,14 +67,43 @@ export default function ProductsList() {
         adresse_facturation: adresseFacturation,
         frais_de_livraison: Number(fraisDeLivraison),
         date: new Date().toISOString(),
-        produits: produitsCommandes.map(p => ({ idProduit: p.idProduit, quantite: p.quantite }))
+        produits: produitsCommandesCreate.map(p => ({ idProduit: p.idProduit, quantite: p.quantite }))
       };
 
       const res = await axios.post(`${API_URL}/orders/order`, body);
       console.log("Commande envoyée :", body);
       toast.success('Commande passée avec succès ! ' + res.data.orderId);
 
-      setProduitsCommandes([]);
+      setProduitsCommandesCreate([]);
+      setAdresseLivraison('');
+      setAdresseFacturation('');
+      setFraisDeLivraison(0);
+    } catch (err) {
+      console.error(err);
+      alert('Erreur lors de la commande. Veuillez réessayer.');
+    }
+  };
+
+  const orderEdit = async () => {    
+    if (!adresseLivraison || !adresseFacturation || produitsCommandesEdit.length === 0) {
+      toast.error('Veuillez remplir toutes les informations nécessaires et ajouter au moins un produit');
+      return;
+    }
+
+    try {
+      const body = {
+        adresse_livraison: adresseLivraison,
+        adresse_facturation: adresseFacturation,
+        frais_de_livraison: Number(fraisDeLivraison),
+        date: new Date().toISOString(),
+        produits: produitsCommandesEdit.map(p => ({ idProduit: p.idProduit, quantite: p.quantite }))
+      };
+
+      const res = await axios.post(`${API_URL}/orders/order`, body);
+      console.log("Commande envoyée :", body);
+      toast.success('Commande passée avec succès ! ' + res.data.orderId);
+
+      setProduitsCommandesEdit([]);
       setAdresseLivraison('');
       setAdresseFacturation('');
       setFraisDeLivraison(0);
@@ -83,51 +115,78 @@ export default function ProductsList() {
 
   return (
     <div className="flex flex-row-reverse gap-8 order">
-        <div className='order-table border border-[#cccccc] rounded-lg hover:shadow-md'>
-            <h2 className="flex flex-row justify-between text-2xl font-bold  mts">
-              Ajouter une commande <MdAdd className='w-8 h-8 text-[#cccccc] hover:text-[#14446c]'/>
-            </h2>
+      <div className="flex-col panel">
+          <div className='order-table border border-[#cccccc] rounded-lg hover:shadow-md panel-content'>
+              <h2 className="flex flex-row justify-between text-2xl font-bold  mts  cursor-pointer"
+               onClick={() => setActivePanel(activePanel === 'create' ? null : 'create')}>
+                Ajouter une commande <MdAdd className='w-8 h-8 text-[#cccccc] hover:text-[#14446c]'/>
+              </h2>
 
-            <div className="grid grid-cols-2 gap-4 mts">
-                <textarea
-                className="border p-2 rounded input-padding"
-                placeholder="Adresse de livraison"
-                value={adresseLivraison}
-                onChange={(e) => setAdresseLivraison(e.target.value)}
-                />
-                <textarea
-                className="border p-2 rounded input-padding"
-                placeholder="Adresse de facturation"
-                value={adresseFacturation}
-                onChange={(e) => setAdresseFacturation(e.target.value)}
-                />
-            </div>
 
-            <CommandeTable produitsCommandes={produitsCommandes} removeProduct={removeProduct}   fraisDeLivraison={fraisDeLivraison}
-                  setFraisDeLivraison={setFraisDeLivraison}/>
+              {activePanel === 'create' && (
+              <div>
+                <div className="grid grid-cols-2 gap-4 mts">
+                    <textarea
+                    className="border p-2 rounded input-padding"
+                    placeholder="Adresse de livraison"
+                    value={adresseLivraison}
+                    onChange={(e) => setAdresseLivraison(e.target.value)}
+                    />
+                    <textarea
+                    className="border p-2 rounded input-padding"
+                    placeholder="Adresse de facturation"
+                    value={adresseFacturation}
+                    onChange={(e) => setAdresseFacturation(e.target.value)}
+                    />
+                </div>
 
-            <button
-                onClick={order}
-                className="bg-[#f18c08] text-white text-md rounded-md w-24 h-12 mts cursor-pointer self-end order-btn"
-            >
-                Commander
-            </button>
+                <CommandeTable produitsCommandes={produitsCommandesCreate} 
+                removeProduct={(id) => setProduitsCommandesCreate(prev => prev.filter(p => p.idProduit !== id))}   
+                fraisDeLivraison={fraisDeLivraison}
+                setFraisDeLivraison={setFraisDeLivraison}/>
+
+                <button
+                    onClick={orderCreate}
+                    className="bg-[#f18c08] text-white text-md rounded-md w-24 h-12 mts cursor-pointer self-end order-btn"
+                >
+                    Commander
+                </button>
+              </div>
+            )}
+          </div>
+          <div className='order-table border border-[#cccccc] rounded-lg hover:shadow-md panel-content'>     
+              <h2 className="flex flex-row justify-between text-2xl font-bold  mts cursor-pointer"
+              onClick={() => setActivePanel(activePanel === 'edit' ? null : 'edit')}>
+                Modifier une commande <MdOutlineEdit className='w-8 h-8 text-[#cccccc] hover:text-[#14446c]'/>
+              </h2>
+              {activePanel === 'edit' && (
+                <OrderEditor  
+                  dataProduits={data}
+                  pagination={pagination}
+                  setPagination={setPagination}
+                  globalFilter={globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                  produitsCommandes={produitsCommandesEdit}
+                  setProduitsCommandes={setProduitsCommandesEdit}
+                  /> 
+              )}        
+          </div>
         </div>
       
         <div className='border border-[#cccccc] rounded-lg hover:shadow-md productlist-section'>
             <h2 className="flex flex-row justify-between text-2xl font-bold mb-4 mts r">
               Liste des produits<MdListAlt className='w-8 h-8 text-[#cccccc] hover:text-[#14446c]' />
             </h2>
-
             <ProductTable
                 data={data}
                 globalFilter={globalFilter}
                 setGlobalFilter={setGlobalFilter}
-                addProduct={addProduct}
+                addProduct={activePanel === 'edit' ? addProductEdit : addProductCreate}
                 pagination={pagination}
                 setPagination={setPagination}
             />
-            </div>
         </div>
+      </div>
+        
   );
 }
