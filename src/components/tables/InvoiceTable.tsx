97191438815f246fcae57e9cef45a80cbbb0a58a
HRojotiana <hei.rojotiana@gmail.com>
2025-07-23 13:58:19 +0300

    chore: add orderType to invoice body

--- src/components/tables/InvoiceTable.tsx
+++ src/components/tables/InvoiceTable.tsx
@@ -70,6 +70,7 @@ export default function InvoiceTable({
                 adresseLivraison: data.adresse_livraison,
                 adresseFacturation: data.adresse_facturation,
                 fraisDeLivraison: data.frais_de_livraison,
+                orderType: data.order_type.toUpperCase(),
                 produits,
             };
 
