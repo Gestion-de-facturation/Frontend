c3d44593826164bd69c27492eb902e53649c9220
HRojotiana <hei.rojotiana@gmail.com>
2025-08-05 14:14:39 +0300

    chore: impl logique

--- src/components/stats/lists/RecentInvoices.tsx
+++ src/components/stats/lists/RecentInvoices.tsx
@@ -1,8 +1,35 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 import { ViewAll } from "@/components/buttons/ViewAll";
+import { Order } from "@/utils/types/orderDetails/Order";
 import { LiaEye, LiaEdit } from "react-icons/lia";
 import '@/styles/stats.css';
+import OrderDetails from "@/components/orders/OrderDetails";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const RecentInvoices = () => {
+    const [recentOrders, setRecentOrders] = useState<Order[]>([]);
+    const [selectedCommandeId, setSelectedCommandeId] = useState<string | null>(null);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchRecentOrders = async () => {
+            try {
+                const res = await axios.get(`${API_URL}/orders`);
+                setRecentOrders(res.data.slice(0, 3));
+            } catch (error) {
+                console.error("Erreur lors du chargement des commandes: ", error);
+            }
+        };
+
+        fetchRecentOrders();
+    }, []);
+
     return (
         <div className="border border-[#cccccc] shadow-md hover:shadow-lg rounded-md recent-invoices">
             <div className="flex justify-between">
@@ -10,49 +37,55 @@ export const RecentInvoices = () => {
                 <ViewAll />
             </div>
             <div>
-                <div className="flex justify-between invoices-list">
-                    <div>
-                        <h3 className="font-semibold">FA250724100705823 <span className="text-green-600">livré</span> </h3>
-                        <p>Tanà</p>
-                        <p>24/07/2025</p>
+                {recentOrders.map((order) => (
+                <>
+                    <div 
+                    className="flex justify-between invoices-list"
+                    key={order.id}
+                    >
+                        <div>
+                            <h3 className="font-semibold">
+                                {order.id} {" "} 
+                                <span className={order.statut_livraison === "livré"
+                                    ? "text-green-600" 
+                                    : order.statut_livraison === "en_cours"
+                                    ? "text-blue-600"
+                                    : order.statut_livraison === "annulé"
+                                    ? "text-red-600"
+                                    : "text-orange-600"
+                                }>
+                                    {order.statut_livraison === 'en_cours'
+                                    ? 'en cours' : order.statut_livraison}
+                                </span> 
+                            </h3>
+                            <p>{order.adresse_livraison}</p>
+                            <p>{new Date(order.date).toLocaleDateString()}</p>
+                        </div>
+                        <div className="recent-invoice-amount-container">
+                            <p className="font-semibold">{order.total} Ar</p>
+                        </div>
+                        <div className="flex gap-2 text-[#f18c08] recent-invoice-icons">
+                            <LiaEye
+                            onClick={() => setSelectedCommandeId(order.id)} 
+                            size={20}
+                            className="cursor-pointer"/>
+                            <LiaEdit 
+                            size={20}
+                            onClick={() => {
+                                const id = order.id;
+                                router.push(`/dashboard/forms/update-invoice?id=${id}`);
+                                }}
+                            className="cursor-pointer"/>
+                        </div>
                     </div>
-                    <div className="recent-invoice-amount-container">
-                        <p className="font-semibold">116100 Ar</p>
-                    </div>
-                    <div className="flex gap-2 text-[#f18c08] recent-invoice-icons">
-                        <LiaEye size={20}/>
-                        <LiaEdit size={20}/>
-                    </div>
-                </div>
-                <div className="flex justify-between invoices-list">
-                    <div>
-                        <h3 className="font-semibold">FA250801074249810 <span className="text-green-600">livré</span> </h3>
-                        <p>Alarobia</p>
-                        <p>01/08/2025</p>
-                    </div>
-                    <div className="recent-invoice-amount-container">
-                        <p className="font-semibold">44500 Ar</p>
-                    </div>
-                    <div className="flex gap-2 text-[#f18c08] recent-invoice-icons">
-                        <LiaEye size={20}/>
-                        <LiaEdit size={20}/>
-                    </div>
-                </div>
-                <div className="flex justify-between invoices-list">
-                    <div>
-                        <h3 className="font-semibold">FA250804143606121 <span className="text-blue-600">en cours</span> </h3>
-                        <p>Ambohitsaina</p>
-                        <p>	04/08/2025</p>
-                    </div>
-                    <div className="recent-invoice-amount-container">
-                        <p className="font-semibold">64500 Ar</p>
-                    </div>
-                    <div className="flex gap-2 text-[#f18c08] recent-invoice-icons">
-                        <LiaEye size={20}/>
-                        <LiaEdit size={20}/>
-                    </div>
-                </div>
+                </>
+                ))}
             </div>
+
+            {selectedCommandeId && (
+                <OrderDetails orderId={selectedCommandeId} onClose={() => setSelectedCommandeId(null)} />
+            )}
+            
         </div>
     )
 }
