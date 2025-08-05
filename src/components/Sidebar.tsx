a90f405e00b4b46ead861bf06e833c6a38bb09a1
HRojotiana <hei.rojotiana@gmail.com>
2025-08-05 08:55:02 +0300

    chore: minimized sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,55 +1,110 @@
 'use client';
 
 import { useRouter, usePathname } from 'next/navigation';
-import '@/styles/sidebar.css'
-import { LogOut } from 'lucide-react';
-import { MdReceipt, MdReceiptLong, MdOutlineAddBox  } from "react-icons/md";
-import {  AiFillProduct } from "react-icons/ai";
-import CreateInvoiceBtn from './buttons/CreateInvoiceBtn';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import CreateInvoiceBtn from './buttons/CreateInvoiceBtn';
+import { LogOut, ChevronLeft, ChevronRight } from 'lucide-react';
+import { MdReceiptLong } from "react-icons/md";
+import { AiFillProduct } from "react-icons/ai";
+import '@/styles/sidebar.css'
 
 export default function Sidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const [isMinimized, setIsMinimized] = useState(false);
+
+    useEffect(() => {
+        const stored = localStorage.getItem('sidebarMinimized');
+        if (stored === 'true') {
+            setIsMinimized(true);
+        }
+    }, []);
+
+    const toggleSidebar = () => {
+        const newState = !isMinimized;
+        setIsMinimized(newState);
+        localStorage.setItem('sidebarMinimized', newState.toString());
+    }
 
     return (
-        <aside className="flex flex-col w-48 bg-gray-100 text-white h-screen p-4 border-r-1 border-[#CCCCCC] side-bar-container fixed top-0 left-0 z-50">
-            <div className='border-b-1 border-[#CCCCCC] h-16'>
-                <h2 className="text-3xl font-semibold place-self-center logo">
-                    <strong className='text-[#14446c]'>Best</strong>
-                    <strong className='text-[#f18c08]'>place</strong>
-                </h2>
+        <aside className={`flex flex-col  ${isMinimized ? 'w-20' : 'w-48'} bg-gray-100 text-white h-screen p-4 border-r-1 border-[#CCCCCC] side-bar-container fixed top-0 left-0 z-50`}>
+
+            <div className="flex">
+                <div className="flex ustify-end minimisation-btn">
+                    <button
+                        onClick={toggleSidebar}
+                        className='text-[#14446c] hover:text-[#f18c08] transition'
+                        title={isMinimized ? "Agrandir" : "Réduire"}
+                    >
+                        {isMinimized ? <ChevronRight /> : <ChevronLeft />}
+                    </button>
+                </div>
+
+                <div className='border-b-1 border-[#CCCCCC] h-16'>
+                    <h2 className="text-3xl font-semibold place-self-center logo">
+                        {isMinimized ? (
+                            <>
+                                <strong className='text-[#14446C]'>B</strong>
+                                <strong className='text-[#f18c08]'>P</strong>
+                            </>
+                        ) : (
+                            <>
+                                <strong className='text-[#14446c]'>Best</strong>
+                                <strong className='text-[#f18c08]'>place</strong>
+                            </>
+                        )}
+                    </h2>
+                </div>
             </div>
+
             <ul className="place-self-center">
                 <div className='sidebar-links mt-link'>
-                   <li><Link href="/dashboard/forms/create_invoice"><CreateInvoiceBtn /></Link></li> 
+                    <li>
+                        <Link 
+                        href="/dashboard/forms/create_invoice">
+                            <CreateInvoiceBtn minimized={isMinimized} />
+                        </Link>
+                    </li>
                 </div>
                 <div className='sidebar-links mt-link'>
-                    <li><Link 
-                    href="/dashboard" 
-                    className={`flex text-[#14446c] text-lg side-bar-content hover:text-[#f18c08] ${
-                        pathname === "/dashboard" ? 'text-[#f18c08]' : 'text-[#14446c]'
-                    }`}><AiFillProduct className='w-5 h-5 link-icon'/>Produits</Link></li>
+                    <li>
+                        <Link
+                            href="/dashboard"
+                            className={`flex text-[#14446c] text-lg side-bar-content hover:text-[#f18c08] ${pathname === "/dashboard" ? 'text-[#f18c08]' : 'text-[#14446c]'
+                                }`}
+                            title={isMinimized ? "Produits" : ""}>
+                            <AiFillProduct className={`${isMinimized ? 'w-9 h-9 minimized-icon': 'w-5 h-5'} link-icon`} />
+                            {!isMinimized && "Produits"}
+                        </Link>
+                    </li>
                 </div>
                 <div className='sidebar-links'>
-                    <li><Link 
-                    href="/dashboard/invoices" 
-                    className={`flex text-[#14446c]  text-lg side-bar-content hover:text-[#f18c08] ${
-                        pathname === "/dashboard/invoices" ? 'text-[#f18c08]' : 'text-[#14446c]'
-                    }`}><MdReceiptLong className='w-5 h-5 link-icon'/>Commandes</Link></li>
+                    <li>
+                        <Link
+                            href="/dashboard/invoices"
+                            className={`flex text-[#14446c]  text-lg side-bar-content hover:text-[#f18c08] ${pathname === "/dashboard/invoices" ? 'text-[#f18c08]' : 'text-[#14446c]'
+                                }`}
+                            title={isMinimized ? "Commandes" : ""}>
+                            <MdReceiptLong className={`${isMinimized ? 'w-9 h-9 minimized-icon': 'w-5 h-5'} link-icon`} />
+                            {!isMinimized && "Commandes"}
+                        </Link>
+                    </li>
                 </div>
             </ul>
+
             <div className='logout-btn-container'>
                 <button
-                className=" flex gap-1 bg-transparent text-red-600 font-semibold p-2 cursor-pointer rounded-md h-10 w-36 disconnect-btn"
-                onClick={() => {
-                localStorage.removeItem('isLoggedIn');
-                router.push('/login');
-                }}
+                    className=" flex gap-1 bg-transparent text-red-600 font-semibold p-2 cursor-pointer rounded-md h-10 w-36 disconnect-btn"
+                    onClick={() => {
+                        localStorage.removeItem('isLoggedIn');
+                        router.push('/login');
+                    }}
                 >
-                <LogOut/> Déconnexion
+                    <LogOut /> 
+                    {!isMinimized && "Déconnexion"}
                 </button>
             </div>
         </aside>
-    )
+    );
 }
--- src/components/buttons/CreateInvoiceBtn.tsx
+++ src/components/buttons/CreateInvoiceBtn.tsx
@@ -1,11 +1,20 @@
 import { IoMdAdd } from "react-icons/io";
 import '@/styles/button.css'
 
-export default function CreateInvoiceBtn () {
+type Props = {
+    minimized: boolean;
+}
+
+export default function CreateInvoiceBtn ({minimized} : Props) {
     return (
-        <button className="flex bg-[#14446c] font-semibold h-10 gap-2 rounded create-inv-btn-container cursor-pointer hover:bg-[#14446ccb]">
-            <IoMdAdd className="create-inv-btn-icon"/>
-            <p className="create-inv-btn-title">Nouvelle commande</p>
+        <button 
+        className={`flex bg-[#14446c] item-center font-semibold h-10 ${
+                minimized ? 'justify-center minimized-width' : 'justify-start gap-2'
+            } rounded create-inv-btn-container cursor-pointer hover:bg-[#14446ccb]`}
+        title={minimized ? 'Ajouter une commande' : ''}
+        >
+            <IoMdAdd className={`${minimized ? 'w-6 h-6 minimized-btn-add' : 'w-4 h-4'} create-inv-btn-icon`}/>
+            {!minimized && <p className="create-inv-btn-title">Nouvelle commande</p>}
         </button>
     )
 }
