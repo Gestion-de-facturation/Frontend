11b15c2d40501c886026b125248747910d1eb5e6
HRojotiana <hei.rojotiana@gmail.com>
2025-08-11 16:13:37 +0300

    Revert "chore: all stats padding inline start -> 0.5vw"
    This reverts commit f91d575d290e80c2af950b071818eeb56cdcfcf8.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function Stats () {
     return (
         <div className="mts">
              <h2 className="text-2xl font-bold dashboard-title">Tableau de bord</h2>
-            <div className="flex justify-evenly gap-2 mts all-stats">
+            <div className="flex justify-evenly gap-2 mts">
                 <TotalInvoices />
                 <PaidInvoices />
                 <TotalRevenue />
