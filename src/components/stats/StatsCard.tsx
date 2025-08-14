de56f672e3da7ab9d6ef8fb12405ec0f8f504271
HRojotiana <127602223+HRojotiana@users.noreply.github.com>
2025-08-14 10:03:10 +0300

    dashboard responsive attempt (#7)
    * change: router.push -> Link
    
    * fix: CreateInvoiceBtn style after loading
    
    * chore: add button.css to button
    
    * fix: css file -> module
    
    * css modules
    
    * moved css module
    
    * refactor: big-screen/styles -> responsive.css
    
    * chore: responsive dashboard

--- src/components/stats/StatsCard.tsx
+++ src/components/stats/StatsCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import '@/styles/stats.css';
-import '@/styles/big-screen/stats.css';
 
 type Props = {
     title: string;
@@ -11,7 +10,7 @@ type Props = {
 
 export default function StatsCard({title, icon, content, percentage}:Props) {
     return (
-        <div className="border border-[#cccccc] w-[25%] 2xl:w-[20%] h-[20vh] 2xl:h-[10vh] shadow-sm rounded-md hover:shadow-md stats-container">
+        <div className="border border-[#cccccc] w-[25%] 2xl:w-[20%] h-[20vh] 2xl:h-[11.5vh] shadow-sm rounded-md hover:shadow-md stats-container">
             <div className="flex justify-between">
                 <p className="font-semibold">{title}</p>
                 <div>{icon}</div>
