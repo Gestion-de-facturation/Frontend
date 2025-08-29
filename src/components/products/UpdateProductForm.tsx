789b0219c8b81c5e8a8d4ab56024451eae27ad3b
HRojotiana <hei.rojotiana@gmail.com>
2025-08-29 08:43:40 +0300

    chore: add spinner in save product updates

--- src/components/products/UpdateProductForm.tsx
+++ src/components/products/UpdateProductForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProductRef from './ProductRef';
 import ProductDetails from './ProductDetails';
 import ProductButton from './ProductButton';
@@ -9,6 +9,8 @@ import { fetchProduct } from '@/utils/handlers/products/update-form/fetchProduct
 import { updateProduct } from '@/utils/handlers/products/update-form/updateProduct';
 import { handleChange } from '@/utils/handlers/products/update-form/handleChange';
 import toast from 'react-hot-toast';
+import { useLoading } from '@/store/useLoadingStore';
+import LoadingOverlay from '../spinners/LoadingOverlay';
 import '@/styles/order.css';
 import '@/styles/product.css';
 
@@ -18,21 +20,22 @@ interface UpdateProductFormProps {
 
 export default function UpdateProductForm({ productId }: UpdateProductFormProps) {
     const [product, setProduct] = useState<Produit | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
+    const { isLoading, show, hide } = useLoading();
 
     useEffect(() => {
         async function loadProduct() {
             try {
+                show();
                 const data = await fetchProduct(productId);
                 setProduct(data);
             } catch (err) {
                 toast.error("Impossible de charger le produit");
             } finally {
-                setLoading(false);
+                hide();
             }
         }
         loadProduct();
-    }, [productId]);
+    }, [productId, show, hide]);
 
     const handleSave = async () => {
         if (!product) return;
@@ -40,23 +43,28 @@ export default function UpdateProductForm({ productId }: UpdateProductFormProps)
         const productToSend = {
             nom: product.nom,
             prixUnitaire: product.prixUnitaire,
-            idCategorie: product.categorie.id,     
-            idFournisseur: product.fournisseur.id  
+            idCategorie: product.categorie.id,
+            idFournisseur: product.fournisseur.id
         };
 
         console.log(`Product to send: ${JSON.stringify(productToSend, null, 2)}`);
 
         try {
+            show();
             await updateProduct(productId, productToSend);
             toast.success("Produit mis à jour avec succès");
         } catch (err) {
             toast.error("Echec de la mise à jour du  produit");
+        } finally {
+            hide();
         }
     };
-    if (loading) return <p>Chargement...</p>;
+
     if (!product) return <p>Produit introuvable.</p>
+
     return (
         <div className='mts product-modif-form w-[80%]'>
+            <LoadingOverlay />
             <h1 className="text-3xl font-bold">Modification produit</h1>
             <ProductRef reference={product.id} />
             <ProductDetails
