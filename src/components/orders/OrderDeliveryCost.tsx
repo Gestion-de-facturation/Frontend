1b6a4ec4e5230ac18c98d408487a996cb28e8ed4
HRojotiana <hei.rojotiana@gmail.com>
2025-09-22 11:54:06 +0300

    chore: add thousands separator

--- src/components/orders/OrderDeliveryCost.tsx
+++ src/components/orders/OrderDeliveryCost.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import '@/styles/form.css';
 import '@/styles/order.css';
+import { formatPrice } from '@/utils/functions/formatPrice';
 
 type Props = {
     fraisDeLivraison: string;
     setFraisDeLivraison: (value: string) => void;
 }
 
-export default function OrderDeliveryCost ({
+export default function OrderDeliveryCost({
     fraisDeLivraison,
     setFraisDeLivraison
-} : Props) {
+}: Props) {
     return (
         <div className="order-delivery-cost-container border border-[#cccccc] shadow-sm rounded-md">
             <h2 className="text-xl font-bold">Frais de livraison</h2>
-            <input 
-            type="number" 
-            value={fraisDeLivraison}
-            placeholder='0'
-            onChange={(e) => setFraisDeLivraison(e.target.value)}
-            className="border p-2 rounded mts w-32 h-8 form-input-padding-left" />
+            <input
+                type="text"
+                value={formatPrice(fraisDeLivraison)}
+                placeholder='0'
+                onChange={(e) => setFraisDeLivraison(e.target.value.replace(/\s/g, ''))}
+                className="border p-2 rounded mts w-32 h-8 form-input-padding-left" />
             <span className='span-ml'>Ar</span>
         </div>
     )
--- src/components/orders/OrderSummary.tsx
+++ src/components/orders/OrderSummary.tsx
@@ -1,4 +1,5 @@
 import '@/app/responsive.css';
+import { formatPrice } from '@/utils/functions/formatPrice';
 
 type Props = {
     totalProduits: number;
@@ -19,19 +20,19 @@ export const OrderSummary = (
             {/** Sous-total */}
             <div className="flex mb-4 form-content-mt gap-2 w-80 h-10 mts">
                 <label className="w-64 font-medium mts"> Sous-total:  </label>
-                <p className="w-64 h-8  p-2 rounded  mts ">{totalProduits} Ar</p>
+                <p className="w-64 h-8  p-2 rounded  mts ">{formatPrice(totalProduits)} Ar</p>
             </div>
 
             {/* Frais de livraison */}
             <div className="flex mb-4 form-content-mt gap-2 w-80 h-10 mts">
                 <label className="w-64 font-medium">Frais de livraison:  </label>
-                <p className="w-64 h-8  p-2 rounded font-semi-bold">{fraisDeLivraison || '0'} Ar</p>
+                <p className="w-64 h-8  p-2 rounded font-semi-bold">{formatPrice(fraisDeLivraison) || '0'} Ar</p>
             </div>
 
             {/**Total */}
             <div className="flex justify-between border-t-2 border-[#cccccc] font-semibold text-lg total-container">
                 <label>Total</label>
-                <p>{total} Ar</p>
+                <p>{formatPrice(total)} Ar</p>
             </div>
         </div>
     );
